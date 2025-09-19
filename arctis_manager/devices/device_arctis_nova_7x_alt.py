e8e6bb2b960b7a27a4a3b0b39cd1b0049b1a4e96
Alody <tmahour@hotmail.com>
2025-09-19 08:10:34 -0400

    Update device_arctis_nova_7x_alt.py

--- arctis_manager/devices/device_arctis_nova_7x_alt.py
+++ arctis_manager/devices/device_arctis_nova_7x_alt.py
@@ -1,7 +1,7 @@
 from arctis_manager.device_manager.device_manager import device_manager_factory
-from arctis_manager.devices.device_arctis_nova_7x import ArctisNova7XDevice
+from arctis_manager.devices.device_arctis_nova_7x import ArctisNova7XWirelessDevice
 
 
 @device_manager_factory(0x2258, 'Arctis Nova 7X Wireless Alt')
-class ArctisNova7XDeviceAlt(ArctisNova7XDevice):
+class ArctisNova7XDeviceAlt(ArctisNova7XWirelessDevice):
     pass
